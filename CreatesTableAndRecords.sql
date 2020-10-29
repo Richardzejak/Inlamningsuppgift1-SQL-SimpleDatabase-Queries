104ad184d9acc8d788118bb3c052a51af99b2fbf
Calphonar <Richardzejak@hotmail.com>
2020-10-29 11:29:05 +0100

    Fixed Primary Keys, switched some data-types and added a couple of records

--- CreatesTableAndRecords.sql
+++ CreatesTableAndRecords.sql
@@ -1,24 +1,36 @@
 DROP TABLE IF EXISTS Movies;
-DROP TABLE IF EXISTS Customer;
+DROP TABLE IF EXISTS Customers;
 DROP TABLE IF EXISTS Rentals;
 
 CREATE TABLE Movies(
-			movie_title TEXT NOT NULL PRIMARY KEY,
+			movie_id INTEGER PRIMARY KEY,
+			movie_title TEXT,
 			stock TEXT,
-			genre TEXT,
-			director TEXT,
-			main_actor TEXT);
-
-CREATE TABLE Customer(
-			customer_id TEXT NOT NULL PRIMARY KEY,
-			username TEXT,
+			genre TEXT);
+			
+			
+CREATE TABLE Customers(
+			customer_id INTEGER NOT NULL PRIMARY KEY,
+			username TEXT UNIQUE,
 			password TEXT,
-			email TEXT);
+			email TEXT UNIQUE);
 
 CREATE TABLE Rentals(
-			order_id TEXT NOT NULL PRIMARY KEY,
-			customer_id TEXT,
-			movie_title TEXT,
-			FOREIGN KEY (customer_id) REFERENCES Customer(customer_id),
-			FOREIGN KEY (movie_title) REFERENCES Movies(movie_title)
+			order_id INTEGER PRIMARY KEY,
+			customer_id INTEGER,
+			movie_id INTEGER,
+			FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+			FOREIGN KEY (movie_id) REFERENCES Movies(movie_id)
 			);
+		
+INSERT INTO Movies(movie_title, stock, genre)
+VALUES
+("Die Hard", "3", "Action"),
+("Empire Strikes back", "2", "Sci-Fi"),
+("Spirited Away", "0", "Anime"),
+("The Shining", "5", "Horror");
+
+INSERT INTO Customers(username, password, email)
+VALUES
+("John", "Doe", "Johndoe@hotmail.com"),
+("Robert","Jonsen","Robbyjoe@hotmail.com");
